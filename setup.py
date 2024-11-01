4fb070054934d644237e9bb9caeb7c80a3a64043
Cheenasa <138380358+Cheenasa@users.noreply.github.com>
2024-10-31 20:49:35 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='your_app_name',
+    version='0.1',
+    install_requires=[
+        'streamlit~=1.39.0',
+        'setuptools',
+        'pandas~=2.0.3',
+        'plotly~=5.9.0',
+        'xlrd~=2.0.1',
+        'numpy~=1.24.3',
+        'seaborn~=0.12.2',
+        'matplotlib~=3.7.2',
+        'altair~=5.4.1',
+        'openpyxl~=3.0.10',
+    ],
+)
